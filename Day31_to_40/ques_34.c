09aa8869a57507dc6348d79bf5052f6c71681d7b
Kumar Parth <jhaparth152@gmail.com>
2026-03-07 23:20:42 +0530

    Add files via upload

---
+++ Day31_to_40/ques_34.c
@@ -0,0 +1,88 @@
+/*Problem: Evaluate Postfix Expression - Implement using linked list with dynamic memory allocation.
+
+Input:
+- Postfix expression with operands and operators
+
+Output:
+- Print the integer result
+
+Example:
+Input:
+2 3 1 * + 9 -
+
+Output:
+-4
+
+Explanation:
+Use stack to store operands, apply operators by popping operands, push result back. Final stack top is result.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+void push(int x) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->next = top;
+    top = newNode;
+}
+
+int pop() {
+    if (top == NULL) {
+        printf("Stack Underflow\n");
+        exit(1);
+    }
+    struct Node* temp = top;
+    int val = temp->data;
+    top = top->next;
+    free(temp);
+    return val;
+}
+
+int main() {
+    char postfix[100];
+    int i = 0;
+
+    printf("Enter postfix expression: ");
+    fgets(postfix, sizeof(postfix), stdin);
+
+    while (postfix[i] != '\0') {
+
+        if (isdigit(postfix[i])) {
+            int num = 0;
+            while (isdigit(postfix[i])) {
+                num = num * 10 + (postfix[i] - '0');
+                i++;
+            }
+            push(num);
+        }
+
+        else if (postfix[i] == '+' || postfix[i] == '-' || postfix[i] == '*' || postfix[i] == '/') {
+            int b = pop();
+            int a = pop();
+            int result;
+
+            switch (postfix[i]) {
+                case '+': result = a + b; break;
+                case '-': result = a - b; break;
+                case '*': result = a * b; break;
+                case '/': result = a / b; break;
+            }
+
+            push(result);
+        }
+
+        i++;
+    }
+
+    printf("%d\n", pop());
+    return 0;
+}
