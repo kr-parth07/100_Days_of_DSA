8db06bed31192a4c204bde85c0bf5517c4b93202
Kumar Parth <jhaparth152@gmail.com>
2026-04-26 23:03:28 +0530

    Add files via upload

---
+++ Day81_to_90/ques_81.c
@@ -0,0 +1,46 @@
+/*Problem: Implement Bubble Sort - Implement the algorithm.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the sorted array or search result
+
+Example:
+Input:
+5
+64 34 25 12 22
+
+Output:
+12 22 25 34 64
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n - 1; i++) {
+        for(j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
---
+++ Day81_to_90/ques_83.c
@@ -0,0 +1,50 @@
+/*Problem: Implement Selection Sort - Implement the algorithm.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the sorted array or search result
+
+Example:
+Input:
+5
+64 34 25 12 22
+
+Output:
+12 22 25 34 64
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, j, min_index, temp;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n - 1; i++) {
+        min_index = i;
+
+        for(j = i + 1; j < n; j++) {
+            if(arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+
+        temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
